d0cf4a682d979956cc6bfd78fa84bad695ef4c7f
Rahul Jadhav <jadhav.rj.cmos@gmail.com>
2023-12-31 14:51:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,49 +3,49 @@ const fs = require('fs/promises')
 const myFileWriter = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
-	 try {
-        await fs.writeFile(fileName, fileContent);
-        console.log(`File ${fileName} created successfully.`);
-    } catch (error) {
-        console.error(`Error creating file ${fileName}: ${error.message}`);
-    }
+try {
+    await fs.writeFile(fileName, fileContent);
+    console.log(`File ${fileName} created successfully.`);
+  } catch (error) {
+    console.error(`Error creating file ${fileName}: ${error.message}`);
+  }
 }
 
 const myFileReader = async (fileName) => {
 	// write code here
 	// dont chnage function name
-	try {
-        const fileContent = await fs.readFile(fileName, 'utf8');
-        return fileContent;
-    } catch (error) {
-        console.error(`Error reading file ${fileName}: ${error.message}`);
-        return null;
-    }
+  try {
+    const fileContent = await fs.readFile(fileName, 'utf8');
+    return fileContent;
+  } catch (error) {
+    console.error(`Error reading file ${fileName}: ${error.message}`);
+    return null;
+  }
 }
 
 
 const myFileUpdater = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
-	 try {
-        const currentContent = await fs.readFile(fileName, 'utf8');
-        const updatedContent = currentContent + newContent;
-        await fs.writeFile(fileName, updatedContent);
-        console.log(`File ${fileName} updated successfully.`);
-    } catch (error) {
-        console.error(`Error updating file ${fileName}: ${error.message}`);
-    }
+ try {
+    const currentContent = await fs.readFile(fileName, 'utf8');
+    const updatedContent = currentContent + newContent;
+    await fs.writeFile(fileName, updatedContent);
+    console.log(`File ${fileName} updated successfully.`);
+  } catch (error) {
+    console.error(`Error updating file ${fileName}: ${error.message}`);
+  }
 }
 
 const myFileDeleter = async (fileName) => {
 	// write code here
 	// dont chnage function name
-	try {
-        await fs.unlink(fileName);
-        console.log(`File ${fileName} deleted successfully.`);
-    } catch (error) {
-        console.error(`Error deleting file ${fileName}: ${error.message}`);
-    }
+  try {
+    await fs.unlink(fileName);
+    console.log(`File ${fileName} deleted successfully.`);
+  } catch (error) {
+    console.error(`Error deleting file ${fileName}: ${error.message}`);
+  }
 }
 
 
